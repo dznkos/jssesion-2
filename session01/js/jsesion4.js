e17d75a6319b5a8d6d49de0f3bc04abbe6dd670b
Luis E. Villavicencio Torres <khanccer@gmail.com>
2022-11-05 12:10:45 -0500

    sesion02 v1.23

--- session01/js/jsesion4.js
+++ session01/js/jsesion4.js
@@ -52,7 +52,7 @@ console.log("Faltan " + dias + " dias para navidad");
 // a. la nota mayor
 // b. la nota menor
 // c. el promedio de notas.
-function eje1 () {
+function eje3 () {
 let notas = [];
 let suma = 0;
 let mayor = 0;
@@ -71,4 +71,139 @@ for(let i=0;i<5;i++){
 console.log("Promedio: " + suma/notas.length);
 console.log("Nota mayor: " + mayor);
 console.log("Nota menor: " + menor);
+}
+
+// Propuesto01.
+// En un evento de 5 dias (lunes 7 nov al viernes 11 nov) 
+// se desea colocar exposiciones de 1 o 2 horas maximo entre horarios de 9am a 8pm.
+// Hacer un programa que permita reservar espacios de tiempo que esten dentro del rango permitido y que no permita traslapes.
+// Al final se debera mostrar las fechas y horarios reservados.
+
+
+var eventodia1 = []
+var eventodia2 = []
+var eventodia3 = []
+var eventodia4 = []
+var eventodia5 = []
+
+
+function ejepro1(){
+
+  // Debe ingresar rangos dia( 7-11) hora (1-2) horario(9-20)(9am.8pm)
+  var reserva =  prompt('Consulte reserva en orden dia(7nv-11nv), rghora (1,2) hora inicio(9am-20pm) : ')
+
+  var resvalues = reserva.split(',')
+  var dia = resvalues[0]
+  var rghora = parseInt(resvalues[1])
+  var hini = parseInt(resvalues[2])
+
+  //validar hora
+  if ((hini < 9 || hini > 19) || (hini == 19 && rghora == 2)) {
+    console.log('Horario invalido');
+    return;
+  }
+
+  switch (dia) {
+    case '7':      
+      if (eventodia1.includes(hini)) {
+        console.log('horario no disponible')
+      }else {
+        if (rghora == 2) {
+          var hr2 = hini + 1 
+          eventodia1.push(hini)
+          eventodia1.push(hr2)
+          console.log( 'Reserva dia 7nov horas:', formatoHora(eventodia1) );
+        }else {        
+          eventodia1.push(hini)
+          console.log( 'Reserva dia 7nov horas:', formatoHora(eventodia1) );
+        }        
+      }
+      break;
+    case '8':
+      if (eventodia2.includes(hini)) {
+        console.log('horario no disponible')
+      }else {
+        if (rghora == 2) {3
+          var hr2 = hini + 1 
+          eventodia2.push(hini)
+          eventodia2.push(hr2)
+          console.log( 'Reserva registrada 8nov horas:', formatoHora(eventodia2) );
+        }else {
+          eventodia2.push(hini)
+          console.log( 'Reserva registrada 8nov horas:', formatoHora(eventodia2) );
+        }        
+      }
+      break;
+    case '9':
+      if (eventodia3.includes(hini)) {
+        console.log('horario no disponible')
+      }else {
+        if (rghora == 2) {
+          var hr2 = hini + 1 
+          eventodia3.push(hini)
+          eventodia3.push(hr2)
+          console.log( 'Reserva registrada 9nov horas:', formatoHora(eventodia3) );
+        }else {
+          eventodia3.push(hini)
+          console.log( 'Reserva registrada 9nov horas:', formatoHora(eventodia3) );
+        }        
+      }
+      break;
+    case '10':
+      if (eventodia4.includes(hini)) {
+        console.log('horario no disponible')
+      }else {
+        if (rghora == 2) {
+          var hr2 = hini + 1 
+          eventodia4.push(hini)
+          eventodia4.push(hr2)
+          console.log( 'Reserva registrada 10nov horas:', formatoHora(eventodia4) );
+        }else {
+          eventodia4.push(hini)
+          console.log( 'Reserva registrada 10nov horas:', formatoHora(eventodia4) );
+        }        
+      }
+      break;
+    case '11':
+      if (eventodia5.includes(hini)) {
+        console.log('horario no disponible')
+      }else {
+        if (rghora == 2) {
+          var hr2 = hini + 1 
+          eventodia5.push(hini)
+          eventodia5.push(hr2)
+          console.log( 'Reserva registrada 11nov horas:', formatoHora(eventodia5) );
+        }else {
+          eventodia5.push(hini)
+          console.log( 'Reserva registrada 11nov horas:', formatoHora(eventodia5) );
+        }        
+      }
+      break;  
+    default:
+      break;
+  }
+}
+
+function mostrarReservas(){
+  console.log('===== Reservas =====')
+  imprimirReservas(eventodia1, 7)
+  imprimirReservas(eventodia2, 8)
+  imprimirReservas(eventodia3, 9)
+  imprimirReservas(eventodia4, 10)
+  imprimirReservas(eventodia5, 11)
+}
+
+function imprimirReservas(reservas, dia) {
+  if (reservas.length > 0 ) {    
+    console.log(`Reserva del dia ${dia}nov x horas:`, formatoHora(reservas) );
+  }else {    
+    console.log(`Reserva del dia ${dia}nov x horas: No se encontraron reservas`);
+  }
+}
+
+function formatoHora(res){
+  return res.map( rs => {
+    var hora = (rs < 12) ? rs +'am' : rs +'pm' 
+    return hora
+  } )
 }
